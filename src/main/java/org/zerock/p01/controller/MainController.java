4ddad068fb70662577e26090f1e633f3dad934ee
DHS094 <149463451+DHS094@users.noreply.github.com>
2023-11-01 15:50:28 +0900

    test

--- src/main/java/org/zerock/p01/controller/MainController.java
+++ src/main/java/org/zerock/p01/controller/MainController.java
@@ -1,18 +1,18 @@
-package org.zerock.p01.controller;
-
-import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-@Log4j2
-public class MainController {
-
-    @GetMapping("/")
-    public String mainPage(Model model){
-        model.addAttribute("message", "메인 페이지");
-        return "main";
-    }
-
-}
+//package org.zerock.p01.controller;
+//
+//import lombok.extern.log4j.Log4j2;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//@Log4j2
+//public class MainController {
+//
+//    @GetMapping("/")
+//    public String mainPage(Model model){
+//        model.addAttribute("message", "메인 페이지");
+//        return "main";
+//    }
+//
+//}
